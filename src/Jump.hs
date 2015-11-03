4d9cf065374c18a46ea53224b5cfe474a33f6d1a
Greg Wiley <gwiley@cj.com>
2015-11-03 10:24:25 -0800

    remove goto fn

--- src/Jump.hs
+++ src/Jump.hs
@@ -6,12 +6,6 @@ import Control.Monad.Identity
 label :: (MonadCont m) => m (m a)
 label = callCC $ \k -> let f = k f in return f
 
--- goto :: (MonadCont m) => m (m a) -> m (m a)
--- goto moment = do
---   jump <- moment
---   moment
-
-
 demoJump :: IO ()
 demoJump = flip runContT return $ do
   lift $ putStrLn "Start"
