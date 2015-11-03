ef079b96d8cc1ed68f26c36bad3416b9767472e7
Greg Wiley <gwiley@cj.com>
2015-11-03 10:17:50 -0800

    initial jump implementation

--- src/Jump.hs
+++ src/Jump.hs
@@ -1,1 +1,21 @@
 module Jump where
+
+import Control.Monad.Cont
+import Control.Monad.Identity
+
+rightNow :: (MonadCont m) => m (m a)
+rightNow = callCC $ \k -> let f = k f in return f
+
+goWhen :: (MonadCont m) => m (m a) -> m (m a)
+goWhen moment = do
+  jump <- moment
+  moment
+
+
+demoJump :: IO ()
+demoJump = flip runContT return $ do
+  lift $ putStrLn "Start"
+  label <- rightNow
+  lift $ putStrLn "Spam"
+  goWhen label
+  lift $ putStrLn "Never End"
