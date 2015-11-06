712a505363f406059dfbcdb1fb5a545e76e42eb4
Greg Wiley <gwiley@cj.com>
2015-11-05 17:01:50 -0800

    a litle progress on while

--- src/Jump.hs
+++ src/Jump.hs
@@ -1,15 +1,29 @@
 module Jump where
 
+import Control.Monad.State
 import Control.Monad.Cont
 import Control.Monad.Identity
 
 label :: (MonadCont m) => m (m a)
 label = callCC $ \k -> let f = k f in return f
 
-demoJump :: IO ()
-demoJump = flip runContT return $ do
+demoJump1 :: IO ()
+demoJump1 = flip runContT return $ do
   lift $ putStrLn "Start"
   top <- label
   lift $ putStrLn "Spam"
   top
   lift $ putStrLn "Never End"
+
+
+
+demoJump2 :: Int
+demoJump2 = runCont (evalStateT body 0) id
+  where
+    body = do
+      l1 <- label
+      modify (+1)
+      s <- get
+      if s == 5
+        then return s
+        else l1
--- src/While.hs
+++ src/While.hs
@@ -1,2 +1,42 @@
 module While where
-  
+
+import Control.Monad.Cont
+import Control.Monad.Fix (fix)
+loop' :: (Monad m) => m a -> m a
+loop' body = fix (body >>)
+
+loop :: (MonadCont m) => m a -> m a
+loop body = callCC $ \k -> fix (body >>)
+
+whileDemo :: IO ()
+whileDemo = flip runContT return $ do
+  callCC $ \k -> loop' $ do
+    lift $ putStrLn "Hello"
+    k()
+  lift $ putStrLn "Good-bye"
+    --  callCC $ \k -> k ()
+
+
+-- loop :: (MonadCont m) =>  m (m a)
+
+
+
+-- loop :: MonadCont m => a -> m (m a)
+
+
+-- while :: (MonadCont m) => Bool -> m Bool
+-- while c0 = callCC $ \k -> do
+--   if k
+--     then let f x = k f
+--       in return f
+--     else k False
+
+
+
+-- lef f x = k (x, f) in return (x0, f)
+
+-- label = callCC $ \k -> let f = k f in return f
+
+
+-- getCC' :: MonadCont m => a -> m (a, a -> m b)
+-- getCC' x0 = callCC (\c -> let f x = c (x, f) in return (x0, f)
